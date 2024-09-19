6541755b4be0fa4dafc35c291944e55fa83ce6b6
jasminumbi <jasmin.umbanhowar@gmail.com>
2024-09-18 20:49:24 -0500

    all tests passing

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -121,7 +121,7 @@ public class TechJobs {
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
         if(someJobs.isEmpty()){
-            System.out.println("No Results");
+            System.out.print("No Results");
             return;
     }
         for (HashMap<String, String> jobs : someJobs) {
